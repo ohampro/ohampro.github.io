26550cc3f830a4bff6df65622eb1dcc420a98d7d
ohamhpr <mohammad.haghighi.poor@gmail.com>
2024-05-30 22:48:09 +0300

    Fixes #10 Move work experiences to About.

--- assets/js/bootstrap_scripts.js
+++ assets/js/bootstrap_scripts.js
@@ -3,5 +3,14 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
+const navLinkList = document.querySelectorAll('.nav-link');
+const navLinks = [...navLinkList].map(linkEl => {
+    let path = document.location.pathname.replaceAll('/','');
+    let href = linkEl.href.split('/').pop();
+    if (href == path){
+        linkEl.classList.add('active');
+    }
+});
+
     // const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
     // const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
