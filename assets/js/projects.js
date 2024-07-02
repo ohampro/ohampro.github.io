4a3345ec89a48ab8b20ff48ae7f784ae9c231861
ohamhpr <mohammad.haghighi.poor@gmail.com>
2024-07-02 16:22:37 +0300

    update card-labels messages and style

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -5,6 +5,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 let docItems = document.getElementsByClassName('mv-docItem');
 let foundItems = 0;
 
+topMsg.classList.add('text-success');
 if (params.with == undefined || params.with.length == 0){
     showAllItems();
 
@@ -14,11 +15,14 @@ if (params.with == undefined || params.with.length == 0){
 
     let topMsg = document.getElementById('topMsg');
     if (foundItems > 0){
-        topMsg.innerHTML = `(${foundItems} of ${docItems.length}) Projects Utilizing '${params.with}':`;
+        topMsg.innerHTML = `(${foundItems} of ${docItems.length}) Projects Utilizing '${params.with}'`;
     }else{
+        topMsg.classList.remove('text-success');
+        topMsg.classList.add('text-danger');
+
         foundItems = highlithLabel(docItems, "-Default");
         if (foundItems > 0){
-            topMsg.innerHTML = `Projects possibly utilizing ${params.with}:`;
+            topMsg.innerHTML = `Projects possibly utilizing '${params.with}'`;
         }else{
             topMsg.innerHTML = "Not Found!";
         }        
